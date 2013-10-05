50859e34f702d6a2bcc4f3364f3c555bf064b02f
Ben0xA <web@ben0xa.com>
2013-10-05 17:56:00 -0500

    Gave hoststest.ps1 a usage for PSHosts
    Just added some description to the hsotstest.ps1 file for usage.

--- Scripts and Modules/Scripts/hoststest.ps1
+++ Scripts and Modules/Scripts/hoststest.ps1
@@ -4,6 +4,11 @@ This tests the $PSHosts functionality with PoshSec Framework
 
 AUTHOR
 Ben0xA
+
+$PSHosts Usage
+$PSHosts.GetHosts([bool AllHosts])
+
+Returns PSObject array with whatever columns are listed in the listview.
 #>
 
 # Begin Script Flow
