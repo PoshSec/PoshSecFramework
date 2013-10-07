64a33b0d7d0864f6432492157c2d9f3837868896
Ben0xA <web@ben0xa.com>
2013-10-07 08:09:11 -0500

    Added Export CSV and TXT (Tab Delimited)
    Added support for exporting a psobject as csv and tab delimited txt.

--- poshsecframework/Utility/ExportObject.cs
+++ poshsecframework/Utility/ExportObject.cs
@@ -16,32 +16,35 @@ namespace poshsecframework.Utility
         {
             try
             {
-                StreamWriter outfile = File.CreateText(filename);
-                XmlTextWriter xmlwtr = new XmlTextWriter(outfile);
-                xmlwtr.Formatting = Formatting.Indented;
-                xmlwtr.WriteStartDocument();
-                xmlwtr.WriteStartElement("Collection");
-                foreach(PSObject pobj in CustomObject)
+                if (CustomObject != null && CustomObject.Count > 0)
                 {
-                    xmlwtr.WriteStartElement("PSObject");
-                    foreach (PSNoteProperty prop in pobj.Properties)
+                    StreamWriter outfile = File.CreateText(filename);
+                    XmlTextWriter xmlwtr = new XmlTextWriter(outfile);
+                    xmlwtr.Formatting = Formatting.Indented;
+                    xmlwtr.WriteStartDocument();
+                    xmlwtr.WriteStartElement("Collection");
+                    foreach (PSObject pobj in CustomObject)
                     {
-                        if (prop != null)
+                        xmlwtr.WriteStartElement("PSObject");
+                        foreach (PSNoteProperty prop in pobj.Properties)
                         {
-                            xmlwtr.WriteStartElement("PSNoteProperty");
-                            xmlwtr.WriteAttributeString("Name", prop.Name);
-                            xmlwtr.WriteAttributeString("Value", (prop.Value ?? "").ToString());
-                            xmlwtr.WriteEndElement();
+                            if (prop != null)
+                            {
+                                xmlwtr.WriteStartElement("PSNoteProperty");
+                                xmlwtr.WriteAttributeString("Name", prop.Name);
+                                xmlwtr.WriteAttributeString("Value", (prop.Value ?? "").ToString());
+                                xmlwtr.WriteEndElement();
+                            }
                         }
+                        xmlwtr.WriteEndElement();
                     }
-                    xmlwtr.WriteEndElement();
-                }
 
-                xmlwtr.WriteEndElement();
-                xmlwtr.WriteEndDocument();
-                xmlwtr.Flush();
-                xmlwtr.Close();
-                xmlwtr = null;
+                    xmlwtr.WriteEndElement();
+                    xmlwtr.WriteEndDocument();
+                    xmlwtr.Flush();
+                    xmlwtr.Close();
+                    xmlwtr = null;
+                }                
             }
             catch (Exception e)
             {
@@ -58,12 +61,75 @@ namespace poshsecframework.Utility
 
         public void CSV(Collection<PSObject> CustomObject, String filename)
         {
-
+            ExportDelim(CustomObject, filename, ",");
         }
 
         public void TXT(Collection<PSObject> CustomObject, String filename)
         {
+            ExportDelim(CustomObject, filename, "\t");
+        }
 
+        private void ExportDelim(Collection<PSObject> CustomObject, String filename, String Delimiter)
+        {
+            try
+            {
+                if (CustomObject != null && CustomObject.Count > 0)
+                {
+                    StreamWriter outfile = File.CreateText(filename);
+                    int idx = -1;
+                    foreach (PSObject pobj in CustomObject)
+                    {
+                        idx++;
+                        if (idx == 0)
+                        {
+                            //Header Row
+                            String header = "";
+                            foreach (PSNoteProperty prop in pobj.Properties)
+                            {
+                                if (prop != null)
+                                {
+                                    header += prop.Name + Delimiter;
+                                }
+                            }
+                            if (header.EndsWith(Delimiter))
+                            {
+                                header = header.Substring(0, header.Length - 1);
+                            }
+                            outfile.WriteLine(header);
+                            outfile.Flush();
+                        }
+                        String value = "";
+                        foreach (PSNoteProperty prop in pobj.Properties)
+                        {
+                            if (prop != null)
+                            {
+                                value += prop.Value + Delimiter;
+                            }
+                        }
+                        if (value.EndsWith(Delimiter))
+                        {
+                            value = value.Substring(0, value.Length - 1);
+                        }
+                        outfile.WriteLine(value);
+                        outfile.Flush();
+                    }
+
+                    outfile.Flush();
+                    outfile.Close();
+                    outfile = null;
+                }
+            }
+            catch (Exception e)
+            {
+                if (e.InnerException != null)
+                {
+                    MessageBox.Show("Unable to export object." + Environment.NewLine + e.Message + Environment.NewLine + e.InnerException.Message);
+                }
+                else
+                {
+                    MessageBox.Show("Unable to export object." + Environment.NewLine + e.Message);
+                }
+            }
         }
     }
 }
