d21fdeba55d18497c676c539f92c0d981b969277
Ben0xA <web@ben0xa.com>
2013-10-29 06:14:02 -0500

    Fixing Up SecIISMonitor Missing "+" & Param Loc
    Missing a plus sign on $PSAlert.Add($_.LogDate + " " + $_.cIP + " " +
    $_.URL + " " + $_.Agent,2)
    Import-Module $PSFramework needs to come after any parameter
    definitions.

--- Scripts and Modules/Scripts/SecIISMonitor.ps1
+++ Scripts and Modules/Scripts/SecIISMonitor.ps1
@@ -2,7 +2,6 @@
 .Synopsis
   A PoshSec Framework function that monitors the current IIS log and generates alerts on specified criteria.  The alerts display the log's timestamp, the client IP, the URL accessed, and the user agent string.
 
-
 .PARAMETER IP
   Client IP address to isolate in the log file.  Generates alerts for those records.
   
@@ -24,35 +23,31 @@
 
 #>
 
-Import-Module $PSframework;
-
 param(
-[String]$IP = "",
-[String]$filter = "",
-[Int]$limit = 100,
-[String]$path = "C:\inetpub\logs\LogFiles\W3SVC1\",
-[int]$poll = 10
-
+  [String]$IP = "",
+  [String]$filter = "",
+  [Int]$limit = 100,
+  [String]$path = "C:\inetpub\logs\LogFiles\W3SVC1\",
+  [int]$poll = 10
 )
+
+Import-Module $PSframework
 
 while($true){
   if([System.Net.IPAddress]::TryParse($IP,[ref] $null)){
      Get-SECIISlog -path $path -filter $filter -limit $limit | ForEach-Object {
-       if($_.cIP -eq $IP){
-         $PSAlert.Add($_.cIP + " " + $_.URL + " " + $_.Agent ,2)
-
-         }
+      if($_.cIP -eq $IP){
+        $PSAlert.Add($_.cIP + " " + $_.URL + " " + $_.Agent ,2)
+      }
      }
    }
-else{ 
-  Get-SECIISlog -path $path -filter $filter -limit $limit | ForEach-Object {
-  $PSAlert.Add($_.LogDate + " " $_.cIP + " " + $_.URL + " " + $_.Agent,2)
+  else{ 
+    Get-SECIISlog -path $path -filter $filter -limit $limit | ForEach-Object {
+      $PSAlert.Add($_.LogDate + " " + $_.cIP + " " + $_.URL + " " + $_.Agent,2)
+    }  
   }
   
-}
-
-
-Start-Sleep -Second $poll
+  Start-Sleep -Second $poll
 }
 
 
