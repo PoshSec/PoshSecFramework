ac66105453cc07684eb700d0f1f294fbba1ba23f
Ben0xA <web@ben0xa.com>
2014-02-03 08:29:26 -0600

    Removing Scripts and Modules
    This is no longer needed with the Github API integration!

--- Scripts and Modules/Scripts/System Information/getdrivestest.ps1
+++
@@ -1,7 +0,0 @@
-<#
-.DESCRIPTION
-Tests the Get-Drives module import.
-#>
-
-Write-Output "Testing Get-Drives module..."
-#End Program
--- Scripts and Modules/Scripts/psftest.psm1
+++
@@ -1,9 +0,0 @@
-<#
-.DESCRIPTION
-Tests the Get-Drives module import.
-#>
-
-# Begin Program Flow
-Write-Output "Testing Get-Drives module..."
-Get-Command -Module PoshSecFramework
-#End Program
