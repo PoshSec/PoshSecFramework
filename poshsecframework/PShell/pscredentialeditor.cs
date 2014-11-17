41a78257ed300f4ba0e07a165ddffd4b6a224ceb
Ben0xA <web@ben0xa.com>
2014-11-17 10:02:56 -0600

    Clean Up of the pscredentialeditor

--- poshsecframework/PShell/pscredentialeditor.cs
+++ poshsecframework/PShell/pscredentialeditor.cs
@@ -92,15 +92,7 @@ namespace poshsecframework.PShell
             IntPtr outCredBuffer = new IntPtr();
             uint outCredSize;
             bool save = false;
-            int result = CredUIPromptForWindowsCredentials(ref credui,
-                0,
-                ref authPackage,
-                IntPtr.Zero,
-                0,
-                out outCredBuffer,
-                out outCredSize,
-                ref save,
-                0x1);
+            int result = CredUIPromptForWindowsCredentials(ref credui,0,ref authPackage,IntPtr.Zero,0,out outCredBuffer,out outCredSize,ref save,0x1);
 
             var usernameBuf = new StringBuilder(100);
             var passwordBuf = new StringBuilder(100);
@@ -112,60 +104,14 @@ namespace poshsecframework.PShell
             NetworkCredential netcreds = null;
             if (result == 0)
             {
-                if (CredUnPackAuthenticationBuffer(0, outCredBuffer, outCredSize, usernameBuf, ref maxUserName,
-                                                   domainBuf, ref maxDomain, passwordBuf, ref maxPassword))
+                if (CredUnPackAuthenticationBuffer(0, outCredBuffer, outCredSize, usernameBuf, ref maxUserName, domainBuf, ref maxDomain, passwordBuf, ref maxPassword))
                 {
-                    //TODO: ms documentation says we should call this but i can't get it to work
-                    //SecureZeroMem(outCredBuffer, outCredSize);
-
-                    //clear the memory allocated by CredUIPromptForWindowsCredentials 
                     CoTaskMemFree(outCredBuffer);
                     netcreds = new NetworkCredential(usernameBuf.ToString(), passwordBuf.ToString(), domainBuf.ToString());
                 }
             }
 
             return netcreds;
-        }
-
-        private enum PromptForWindowsCredentialsFlags
-        {
-            /// <summary>
-            /// The caller is requesting that the credential provider return the user name and password in plain text.
-            /// This value cannot be combined with SECURE_PROMPT.
-            /// </summary>
-            CREDUIWIN_GENERIC = 0x1,
-            /// <summary>
-            /// The Save check box is displayed in the dialog box.
-            /// </summary>
-            CREDUIWIN_CHECKBOX = 0x2,
-            /// <summary>
-            /// Only credential providers that support the authentication package specified by the authPackage parameter should be enumerated.
-            /// This value cannot be combined with CREDUIWIN_IN_CRED_ONLY.
-            /// </summary>
-            CREDUIWIN_AUTHPACKAGE_ONLY = 0x10,
-            /// <summary>
-            /// Only the credentials specified by the InAuthBuffer parameter for the authentication package specified by the authPackage parameter should be enumerated.
-            /// If this flag is set, and the InAuthBuffer parameter is NULL, the function fails.
-            /// This value cannot be combined with CREDUIWIN_AUTHPACKAGE_ONLY.
-            /// </summary>
-            CREDUIWIN_IN_CRED_ONLY = 0x20,
-            /// <summary>
-            /// Credential providers should enumerate only administrators. This value is intended for User Account Control (UAC) purposes only. We recommend that external callers not set this flag.
-            /// </summary>
-            CREDUIWIN_ENUMERATE_ADMINS = 0x100,
-            /// <summary>
-            /// Only the incoming credentials for the authentication package specified by the authPackage parameter should be enumerated.
-            /// </summary>
-            CREDUIWIN_ENUMERATE_CURRENT_USER = 0x200,
-            /// <summary>
-            /// The credential dialog box should be displayed on the secure desktop. This value cannot be combined with CREDUIWIN_GENERIC.
-            /// Windows Vista: This value is not supported until Windows Vista with SP1.
-            /// </summary>
-            CREDUIWIN_SECURE_PROMPT = 0x1000,
-            /// <summary>
-            /// The credential provider should align the credential BLOB pointed to by the refOutAuthBuffer parameter to a 32-bit boundary, even if the provider is running on a 64-bit system.
-            /// </summary>
-            CREDUIWIN_PACK_32_WOW = 0x10000000,
         }
     }
 }
