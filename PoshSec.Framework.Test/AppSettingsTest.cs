6421962f3266d7a05199be96065f16804c5edf0e
Mark Good <mark.alan.good@gmail.com>
2018-06-16 22:25:12 -0400

    add AppSettings to read/write settings to settings.json

---
+++ PoshSec.Framework.Test/AppSettingsTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PoshSec.Framework.Test
+{
+    [TestClass]
+    public class AppSettingsTest
+    {
+        class MySettings
+        {
+            public MySettings()
+            {
+                ListOfStringsSetting = new List<string>();
+            }
+            public int IntegerSetting { get; set; }
+            public string StringSetting { get; set; }
+            public List<string> ListOfStringsSetting { get; }
+        }
+
+        [TestMethod]
+        public void SaveSettingsTest()
+        {
+            var settings = new MySettings();
+            settings.IntegerSetting = 3;
+            settings.StringSetting = "four";
+            settings.ListOfStringsSetting.AddRange(new[] { "five", "six" });
+
+            AppSettings<MySettings>.Save(settings);
+            Assert.IsTrue(File.Exists("settings.json"));
+        }
+
+        [TestMethod]
+        public void LoadSettingsTest()
+        {
+            var settings = AppSettings<MySettings>.Load();
+            Assert.IsNotNull(settings);
+        }
+    }
+}
---
+++ poshsecframework/Settings/AppSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace PoshSec.Framework
+{
+    public class AppSettings<TSettings> where TSettings : new()
+    {
+        private const string DEFAULT_FILEPATH = "settings.json";
+
+        public static TSettings Load(string path = DEFAULT_FILEPATH)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException("Settings file not found.", path);
+            var text = File.ReadAllText(path);
+            var settings = JsonConvert.DeserializeObject<TSettings>(text);
+            return settings;
+        }
+
+        public static void Save(TSettings settings, string path = DEFAULT_FILEPATH)
+        {
+            var text = JsonConvert.SerializeObject(settings);
+            File.WriteAllText(path, text);
+        }
+    }
+}
